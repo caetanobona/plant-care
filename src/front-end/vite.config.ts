dc1a5ac80c13c631512555aacfb9d685bc867bbc
Caetano de Medeiros Bona <caetano.bona@gmail.com>
2025-05-15 12:10:56 -0300

    Added Tanstack Router and Tanstack Query + generated users route

--- src/front-end/vite.config.ts
+++ src/front-end/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/plant-care",
+  plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }), react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })

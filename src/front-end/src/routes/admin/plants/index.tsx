a89bdb1fb33d74176e3d6d509ec38003c3361837
Caetano de Medeiros Bona <caetano.bona@gmail.com>
2025-05-15 18:57:41 -0300

    added table to visualize all registered users

--- src/front-end/src/routes/admin/plants/index.tsx
+++ src/front-end/src/routes/admin/plants/index.tsx
@@ -4,10 +4,16 @@ export const Route = createFileRoute('/admin/plants/')({
   component: RouteComponent,
 })
 
+type Plant = {
+  name: string
+  species: string
+  wateringInterval: number
+}
+
 function RouteComponent() {
   return (
     <div>
-      
+      <h1>ADM Plants View</h1>
     </div>
   )
 }
--- src/front-end/src/routes/admin/users/index.tsx
+++ src/front-end/src/routes/admin/users/index.tsx
@@ -1,13 +1,51 @@
+import { AdminTable } from '@/components/ui/admin-table'
 import { createFileRoute } from '@tanstack/react-router'
+import { ColumnDef } from "@tanstack/react-table"
 
 export const Route = createFileRoute('/admin/users/')({
   component: RouteComponent,
 })
 
+export type User = {
+  name: string
+  email: string
+  password: string
+  isActive: boolean
+}
+
+export const tableColumns: ColumnDef<User>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "password",
+    header: "Password",
+  },
+  {
+    accessorKey: "isActive",
+    header: "Active"
+  }
+]
+
+const usersMock : User[] = [
+  {
+    name: "Caetano",
+    email: "caetano@mail.com",
+    password: "password-hash",
+    isActive: true
+  }
+] 
+
 function RouteComponent() {
   return (
     <div>
-      Hello "/users/"!
+      <h1>ADM Users View</h1>
+      <AdminTable columns={tableColumns} data={usersMock}/>
     </div>
   )
 }

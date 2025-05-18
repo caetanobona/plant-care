18e50c6e1713b0426d323970c9b40add2010488d
Caetano de Medeiros Bona <caetano.bona@gmail.com>
2025-05-18 16:37:38 -0300

    Re-created PlantCare.Application Project

--- src/back-end/PlantCare.Domain/obj/Debug/net9.0/PlantCare.Domain.AssemblyInfo.cs
+++ src/back-end/PlantCare.Domain/obj/Debug/net9.0/PlantCare.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlantCare.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89671e8d966e96bd6de9030e047c18ffc1c112b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf67cae3c0284cadb2f224110daed3d0f8c0c6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlantCare.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlantCare.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

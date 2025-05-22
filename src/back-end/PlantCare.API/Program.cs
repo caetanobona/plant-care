0554ce1f93ad2091ea352a761f93628384ba2a11
Caetano de Medeiros Bona <caetano.bona@gmail.com>
2025-05-22 10:58:53 -0300

    Added and implemented Automapper. Completed Program.cs setup

--- src/back-end/PlantCare.API/Program.cs
+++ src/back-end/PlantCare.API/Program.cs
@@ -1,20 +1,37 @@
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using PlantCare.Application.Context;
+using PlantCare.Application.Mapping;
+using PlantCare.Application.Users.Interfaces;
+using PlantCare.Application.Users.Services;
+using PlantCare.Domain.Repositories;
 using PlantCare.Infra.Data;
+using PlantCare.Infra.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();  
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(UserProfile));
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 builder.Services.AddDbContext<IPlantCareDbContext, PlantCareDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
+
+app.UseRouting();  
+app.UseAuthorization(); // If you're using authorization  
+app.MapControllers();
 
 using (var scope = app.Services.CreateScope())
 {
@@ -35,4 +52,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.Run();
+
 

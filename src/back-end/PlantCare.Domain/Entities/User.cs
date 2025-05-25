9829c39097690c0657f45184136b91c2e06caec4
Caetano de Medeiros Bona <caetano.bona@gmail.com>
2025-05-25 16:26:12 -0300

    Added Active and Audition properties to BaseEntity and removed DataAnnotations

--- src/back-end/PlantCare.Domain/Entities/User.cs
+++ src/back-end/PlantCare.Domain/Entities/User.cs
@@ -6,30 +6,9 @@ namespace PlantCare.Domain.Entities;
 
 public class User :  BaseEntity
 {
-    [Required]
-    [MaxLength(320)]
-    public string Email { get; set; }
-    
-    [Required]
-    [MaxLength(100)]
-    public string Name { get; set; }
-    
-    [Required]
-    [MaxLength(50)]
-    public string Username { get; set; }
-    
-    [Required]
-    [MaxLength(255)]
-    public string PasswordHash { get; set; } = null!;
-    
-    [Required]
-    public bool Active { get; set; }
-    
-    [Required]
-    public DateTime CreatedAt { get; set; }
-
-    [Required]
-    public DateTime UpdatedAt { get; set; }
-    
+    public required string Email { get; set; }
+    public required string Name { get; set; }
+    public required string Username { get; set; }
+    public required string PasswordHash { get; set; }
     public virtual ICollection<Plant> Plants { get; set; } = new List<Plant>();
 }

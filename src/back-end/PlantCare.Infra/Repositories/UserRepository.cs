bc65c6f209772f5c4c7d0932819765b5b1e492d3
Caetano de Medeiros Bona <caetano.bona@gmail.com>
2025-05-25 17:05:47 -0300

    Updated UserRepository to use IBaseRepository

--- src/back-end/PlantCare.Infra/Repositories/UserRepository.cs
+++ src/back-end/PlantCare.Infra/Repositories/UserRepository.cs
@@ -1,102 +1,34 @@
 using Microsoft.EntityFrameworkCore;
-using PlantCare.Application.Context;
 using PlantCare.Application.Users.Interfaces;
 using PlantCare.Domain.Entities;
 using PlantCare.Domain.Repositories;
+using PlantCare.Infra.Data;
 
 namespace PlantCare.Infra.Repositories;
 
-public class UserRepository : IUserRepository
+public class UserRepository : BaseRepository<User>, IUserRepository
 {
-
-    private readonly IPlantCareDbContext _dbContext;
-    
-    public UserRepository( IPlantCareDbContext context )
-    {
-        _dbContext = context;
-    }
-    
-    public async Task<User?> InsertAsync(User obj)
-    {
-        var newUser = await _dbContext.Users.AddAsync(obj);
-        await _dbContext.SaveChangesAsync();
-        
-        return newUser.Entity;
-    }
-
-    public async Task<User?> UpdateAsync(User obj)
-    {
-        var updatedUser = _dbContext.Users.Update(obj);
-        
-        await _dbContext.SaveChangesAsync();
-        
-        return updatedUser.Entity;
-    }
-    
-    public async Task<bool> DeleteAsync(long id)
-    {
-        var user = await _dbContext.Users.FindAsync(id);
-
-        if (user == null)
-        {
-            return false;
-        }
-            
-        user.Active = false;
-        _dbContext.Users.Update(user);
-        await _dbContext.SaveChangesAsync();
-
-        return true;
-    }
-
-    public async Task<List<User>> GetAllAsync()
-    {
-        var users = await _dbContext.Users.ToListAsync();
-
-        if (users != null)
-        {
-            Console.WriteLine($"User GetAllAsync Response {users.Count} users");
-        }
-        
-        return users;
-    }
-
-    public async Task<User?> GetByIdAsync(long id)
-    {
-        var user = await _dbContext.Users.FindAsync(id);
-        
-        return user;
-    }
+    public UserRepository(PlantCareDbContext dbContext) : base(dbContext) { }
 
     public async Task<User?> GetByUsername(string username)
     {
-        var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Username == username);
+        var user = await DbSet.FirstOrDefaultAsync(u => u.Username == username);
         
         return user;
     }
     
     public async Task<bool> DoesUsernameExist(string username)
     {
-        var result = await _dbContext.Users.AnyAsync(u => u.Username == username);
-        
-        Console.WriteLine($"checking username {username} = {result}");
+        var result = await DbSet.AnyAsync(u => u.Username == username);
 
         return result;
     }
 
     public async Task<bool> DoesEmailExist(string email)
     {
-        var result = await _dbContext.Users.AnyAsync(u => u.Email == email);
-        
-        Console.WriteLine($"checking email {email} - {result}");
+        var result = await DbSet.AnyAsync(u => u.Email == email);
 
         return result;
     }
 
-    public async Task<bool> ExistsAsync(long id)
-    {
-        var result = await _dbContext.Users.FindAsync(id);
-        
-        return result != null;
-    }
 }

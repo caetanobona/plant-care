9c118eab2c7afb2571fa926b593f95f652c12af2
Caetano de Medeiros Bona <caetano.bona@gmail.com>
2025-05-30 19:34:01 -0300

    Removed DataNotations

--- src/back-end/PlantCare.Domain/Entities/BaseEntity.cs
+++ src/back-end/PlantCare.Domain/Entities/BaseEntity.cs
@@ -4,15 +4,8 @@ namespace PlantCare.Domain.Entities;
 
 public abstract class BaseEntity
 {
-    [Required]
     public virtual long Id { get; set; }
-    
-    [Required]
     public bool Active { get; set; }
-    
-    [Required]
     public DateTime CreatedAt { get; set; }
-
-    [Required]
     public DateTime UpdatedAt { get; set; }
 }

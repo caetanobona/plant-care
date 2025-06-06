651e0df39316bf9f4144e1f6dc72c57c49ae6585
Caetano <caetano.bona@gmail.com>
2025-06-06 16:39:22 -0300

    Created CreatePlantRequestValidator

--- src/back-end/PlantCare.Application/Plants/Validators/CreatePlantRequestValidator.cs
+++ src/back-end/PlantCare.Application/Plants/Validators/CreatePlantRequestValidator.cs
@@ -1,9 +1,31 @@
 using FluentValidation;
+using FluentValidation.Validators;
 using PlantCare.Application.Plants.Models;
 
 namespace PlantCare.Application.Plants.Validators;
 
 public class CreatePlantRequestValidator : AbstractValidator<CreatePlantRequest>
 {
+    public CreatePlantRequestValidator()
+    {
+        RuleFor(request => request.UserId)
+            .NotNull().WithMessage("Plant must have an owner")
+            .IsInEnum().WithMessage("User id must be a number")
+            .GreaterThan(0).WithMessage("User id must be a valid ID");
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required")
+            .MinimumLength(3).WithMessage("Name must be at least 3 characters")
+            .MaximumLength(50).WithMessage("Name cannot exceed 50 characters");
+        RuleFor(x => x.Species)
+            .NotEmpty().WithMessage("Species is required")
+            .MinimumLength(10).WithMessage("Species must be at least 10 characters")
+            .MaximumLength(120).WithMessage("Species cannot exceed 100 characters");
+        RuleFor(x => x.ImageUrl)
+            .MaximumLength(255).WithMessage("ImageUrl cannot exceed 255 characters");
+        RuleFor(x => x.WateringInterval)
+            .NotNull().WithMessage("Plant must have a watering interval");
+        RuleFor(x => x.LightRequirements)
+            .MaximumLength(50).WithMessage("LightRequirements cannot exceed 50 characters");
+    }
     
 }

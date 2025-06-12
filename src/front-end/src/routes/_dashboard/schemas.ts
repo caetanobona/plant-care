8e1a3390e5ac7e8aac78d29d5669e20f6ffc8b11
Caetano <caetano.bona@gmail.com>
2025-06-12 15:31:33 -0300

    removed schemas.ts from routes directory and fixed errors in routeTree.gen.ts

--- src/front-end/src/routes/_dashboard/schemas.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-
-export const registerPlantSchema = z.object({
-  name: z.string().trim().min(1, "Required").max(50, "Maximum 50 characters"),
-  species: z.string().trim().min(10, "Minimum 10 characters required"),
-  imageHash: z.string().base64url(),
-  wateringInterval: z.string().length(10, `Must be in 00:00:00 format`)
-})

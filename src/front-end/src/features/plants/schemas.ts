34516056015b4b3d76e8d5d4e39c7bc1aa74365a
Caetano <caetano.bona@gmail.com>
2025-06-12 16:10:55 -0300

    Update plant schemas.ts

--- src/front-end/src/features/plants/schemas.ts
+++ src/front-end/src/features/plants/schemas.ts
@@ -4,16 +4,19 @@ export const createPlantSchema = z.object({
   userId: z.number().positive(),
   name: z.string().trim().min(1, "Required").max(50, "Maximum 50 characters"),
   species: z.string().trim().min(10, "Minimum 10 characters required"),
-  image: z.string().base64url().nullable(),
-  wateringInterval: z.string().length(10, `Must be in 00:00:00 format`)
+  image: z.string().base64url().nullable().optional(),
+  wateringInterval: z.string().length(8, `Must be in 00:00:00 format`)
 })
 
 export const plantResponseSchema = z.object({
   id: z.number(),
+  userId: z.number(),
   name: z.string(),
   species: z.string(),
-  imageUrl: z.string().nullable(),
-  wateringInterval: z.string()
+  imageHash: z.string().nullable(),
+  wateringInterval: z.string(),
+  lastWatered: z.string().nullable(),
+  lightRequirements: z.string().nullable()
 })
 
 export type CreatePlantRequest = z.infer<typeof createPlantSchema>

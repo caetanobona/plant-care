618420ae44c179363cebf1ec2658accefd0b2654
Caetano <caetano.bona@gmail.com>
2025-06-13 11:24:30 -0300

    Removed rule for maximum length in CreatePlantRequestValidator

--- src/back-end/PlantCare.Application/Plants/Validators/CreatePlantRequestValidator.cs
+++ src/back-end/PlantCare.Application/Plants/Validators/CreatePlantRequestValidator.cs
@@ -19,8 +19,6 @@ public class CreatePlantRequestValidator : AbstractValidator<CreatePlantRequest>
             .NotEmpty().WithMessage("Species is required")
             .MinimumLength(10).WithMessage("Species must be at least 10 characters")
             .MaximumLength(120).WithMessage("Species cannot exceed 100 characters");
-        RuleFor(x => x.ImageHash)
-            .MaximumLength(64).WithMessage("ImageHash cannot exceed 64 characters");
         RuleFor(x => x.WateringInterval)
             .NotNull().WithMessage("Plant must have a watering interval");
         RuleFor(x => x.LightRequirements)
